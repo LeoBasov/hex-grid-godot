5f940c32715e306eac7f5f875c8bea58c2c34eb0
Leo Basov <leo.basov@dlr.de>
2023-05-04 15:23:26 +0200

    added test camera

--- test/test.gd
+++ test/test.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _ready():
 	$HexGrid.position = get_viewport().get_visible_rect().size * 0.5
-	$HexGrid.build(1)
+	$TestCamera2D.position = get_viewport().get_visible_rect().size * 0.5
+	$HexGrid.build(2)
